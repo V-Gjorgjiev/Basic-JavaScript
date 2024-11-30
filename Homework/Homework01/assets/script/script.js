0e05a218b63fc563cac4e650c7d2ed0861cebb56
Vitomir Gjorgjiev <vitomir.gjorgjiev@gmail.com>
2024-11-30 12:16:19 +0100

    first commit

---
+++ Homework/Homework01/assets/script/script.js
@@ -0,0 +1,24 @@
+// Exercise 1
+let lengthInFeet = 15;
+let convertedFromMeters = 0.3048;
+let lengthInMeters = lengthInFeet * convertedFromMeters;
+console.log(lengthInMeters);
+
+// Exercise 2
+let sideA = 10;
+let sideB = 20;
+let area = sideA * sideB;
+console.log(area);
+
+// Exercise 3
+let radius = 2;
+let pi = 3.14159265359
+let areaOfCircle = pi * Math.pow(radius, 2);
+console.log(areaOfCircle);
+
+// Exercise 4
+let pricePerPhone = 119.95;
+let taxRate = 0.05;
+let totalPrice = pricePerPhone * taxRate;
+let totalTaxPrice = totalPrice * 30;
+console.log(totalTaxPrice);
